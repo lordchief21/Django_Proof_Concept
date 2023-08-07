d5f0305618b7becdb140e116c35263e6cf05ab99
Lordchief <lordchief21@gmail.com>
2023-08-07 00:38:34 -0500

    "Set an optional field in Clientes model"

--- GestionPedidos/models.py
+++ GestionPedidos/models.py
@@ -5,13 +5,16 @@ from django.db import models
 class Clientes(models.Model):
     nombre = models.CharField(max_length=30)
     direccion = models.CharField(max_length=50)
-    email = models.EmailField()
+    email = models.EmailField(blank=True, null=True)
     tfno = models.CharField(max_length=7)
 
 class Articulos(models.Model):
     nombre = models.CharField(max_length=30)
     sección = models.CharField(max_length=20)
     precio = models.IntegerField()
+
+    def __str__(self) -> str:
+        return "El artículo %s de la sección %s cuesta %i"%(self.nombre,self.sección,self.precio)
 
 class Pedidos(models.Model):
     numero = models.IntegerField()
